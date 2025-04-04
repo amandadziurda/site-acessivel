ba50d1cec9317cea02aa7ad0ecc13ef2cd11757a
amandadziurda <141340587+amandadziurda@users.noreply.github.com>
2025-04-04 07:40:48 -0300

    .

--- script2.js
+++ script2.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
     let tamanhoAtualFonte = 1;
